0985c9ab3aeeef4828ef9346672771015874b25f
Maxime Biloé <maxime.biloe@gmail.com>
2017-02-01 10:31:53 +0100

    // Version updated to v2.0.0

--- ps_currencyselector.php
+++ ps_currencyselector.php
@@ -39,7 +39,7 @@ class Ps_Currencyselector extends Module implements WidgetInterface
     {
         $this->name = 'ps_currencyselector';
         $this->author = 'PrestaShop';
-        $this->version = '1.0.4';
+        $this->version = '2.0.0';
         $this->need_instance = 0;
 
         parent::__construct();
